a06229a3a1147378363a6d32ad6c08104f4d006f
khanhkhanh1210 <khanh.nguyentom@hcmut.edu.vn>
2024-09-14 11:29:58 +0700

    Initial commit

---
+++ vending machine/vedingmachine_tb.sv
@@ -0,0 +1,63 @@
+module vending_testbench();
+    // Inputs
+    reg clk, rst_n;
+    reg nickle, dime, quarter;
+    // Outputs
+    wire [2:0] change;
+    wire soda;
+    // Outputs for test
+    wire [1:0] state_test;
+    wire [4:0] coin_value_test;
+
+    logic [2:0] possible_coin [0:2]; // Array to hold the possible values
+    logic [2:0] rand_delay; // Random delay value
+    
+    vending_machine uut (
+        .clk(clk), 
+        .rst_n(rst_n), 
+        .nickle(nickle), 
+        .dime(dime), 
+        .quarter(quarter), 
+        .change(change), 
+        .soda(soda),
+        .state_test(state_test),
+        .coin_value_test(coin_value_test)
+    );
+    
+    task randomize_coin();
+        {nickle, dime, quarter} = $urandom_range(0, 2);  // Generate random index
+        {nickle, dime, quarter}= possible_coin[{nickle, dime, quarter}];  // Assign the corresponding value
+    endtask
+
+    // Clock generation
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
+
+    // Reset generation
+    initial begin
+        rst_n = 0;
+        #5 rst_n = 1;
+    end
+
+    task random_delay();
+        rand_delay = $urandom_range(0, 10);  // Random delay between 1 and 10 time units
+        #rand_delay;  // Apply the delay
+    endtask
+    
+    // Randomize the coin value
+    initial begin
+        possible_coin[0] = 3'b001; // 5$
+        possible_coin[1] = 3'b010; // 10$
+        possible_coin[2] = 3'b100; // 25$
+        repeat(50) begin
+            randomize_coin();
+            random_delay();
+            // turn off the coin input
+            {nickle, dime, quarter} = 3'b000;
+            random_delay();
+        end
+        $finish;
+end
+endmodule   
---
+++ vending machine/vending_machine.sv
@@ -0,0 +1,99 @@
+//veding machine
+module vending_machine(
+    input logic clk, rst_n,
+    input logic nickle, dime, quarter,    
+    output logic [2:0] change, // 000: no change, 001: 5$, 010: 10$, 011: 15$, 100: 20$
+    output logic soda,
+// state test
+    output logic [1:0] state_test,
+    output logic [4:0] coin_value_test
+);
+// flag
+    logic enough; // 1: enough money, 0: not enough money
+
+// coin input value
+    logic [4:0] coin;
+
+// coin value
+    logic [4:0] coin_value;
+    logic [4:0] change_value;
+
+// state machine
+    typedef enum logic [1:0] {
+        IDLE,
+        ACCEPT,
+        DISPENSE
+    } state;
+
+// create state machine
+    state current_state, next_state;   
+    always_ff @ (posedge clk or negedge rst_n) begin : state_machine
+        if (!rst_n) current_state <= IDLE;
+        else current_state <= next_state;
+    end
+
+// state transition 
+    always_comb begin
+        state_test = current_state;
+        case (current_state)
+            IDLE: next_state = ACCEPT;
+            ACCEPT: begin
+                if (coin_value >= 5'd20) next_state = DISPENSE;
+                else next_state = ACCEPT;
+            end
+            DISPENSE: next_state = IDLE;
+            default: next_state = IDLE;
+        endcase
+    end
+
+// decode coin value
+    always_comb begin
+        coin_value_test = coin_value;
+        case ({nickle, dime, quarter})
+            3'b001: coin = 5'd5;
+            3'b010: coin = 5'd10;
+            3'b100: coin = 5'd25;
+            default: coin = 5'd0;
+        endcase
+    end
+
+// coin total value
+    always_ff@ (posedge clk or negedge rst_n) begin
+        if (!rst_n || current_state == IDLE) coin_value <= 0;
+        else if (current_state == ACCEPT) coin_value <= coin_value + coin;
+    end
+
+//change decode
+    always_comb begin
+            case (change_value)
+            5'd5: change = 3'b001;
+            5'd10: change = 3'b010;
+            5'd15: change = 3'b011;
+            5'd20: change = 3'b100;
+            default: change = 3'b000;
+        endcase
+    end
+
+// state output
+    always_comb begin
+        case (current_state)
+            IDLE: begin
+                soda = 0;
+                change_value = 0;
+            end
+            ACCEPT: begin
+                soda = 0;
+                change_value = 0;
+            end
+            DISPENSE: begin
+                soda = 1;
+                change_value = coin_value - 5'd20;
+            end
+            default: begin
+                soda = 0;
+                change_value = 0;
+            end
+        endcase
+    end
+
+endmodule
