cb102de319427513dcb4bd2da2451122f018e1a4
Nguyen Thanh Yen Thy <yenthyngth1133@gmail.com>
2024-11-08 18:44:21 +0700

    fix OR opcode

--- Milestone2_Single Cycle/00_src/alu.sv
+++ Milestone2_Single Cycle/00_src/alu.sv
@@ -15,7 +15,7 @@ module alu
     localparam A_ADD  = 4'b0000;
     localparam A_SUB  = 4'b1000;
     localparam A_XOR  = 4'b0100;
-    localparam A_OR   = 4'b0101;
+    localparam A_OR   = 4'b0110;
     localparam A_AND  = 4'b0111;
     localparam A_SLL  = 4'b0001;
     localparam A_SRL  = 4'b0101;
